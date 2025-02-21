c42592c906520dbfa8fde323af9598d4b1dd6e16
daliaabdalla05 <daliaabdalla05@gmail.com>
2025-02-21 17:05:15 +0200

    Added multiple files
    git push origin main#	.gitattributes

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap Test</title>
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+</head>
+<body>
+  <div class="container">
+    <h1 class="text-primary">Bootstrap is working!</h1>
+  </div>
+</body>
+
+<body>
+<div class="card m-4">
+<div class="card-header">Basic Web Page with Bootstrap</div>
+<div class="card-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat consequat enim,
+at tincidunt lacus malesuada eget. Fusce nec magna lectus. Donec sit amet consequat est. Aliquam
+quis augue pharetra, ornare ligula sed, dignissim turpis. Interdum et malesuada fames ac ante ipsum
+ortor</div>
+</div>
+</body>
+</html>
