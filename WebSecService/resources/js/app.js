0741f8750a0b1c8d5c9902b18e5deb15d19a6190
daliaabdalla05 <daliaabdalla05@gmail.com>
2025-05-02 13:31:53 +0300

    UI changes

--- WebSecService/resources/js/app.js
+++ WebSecService/resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+// Auto-hide alerts after 4 seconds
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.alert').forEach(alert => {
+        setTimeout(() => {
+            alert.style.display = 'none';
+        }, 4000);
+    });
+});
